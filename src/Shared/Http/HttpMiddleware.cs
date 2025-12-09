944798b3d3c9de0cf8f4dab765104a2bf1b7f732
jeimm08 <20jeimm23@gmail.com>
2025-12-08 22:58:27 -0400

    Add HttpRouter

---
+++ src/Shared/Http/HttpMiddleware.cs
@@ -0,0 +1,11 @@
+namespace Shared.Http;
+
+using System.Collections;
+using System.Net;
+
+public delegate Task HttpMiddleware(
+    HttpListenerRequest req,
+    HttpListenerResponse res,
+    Hashtable props,
+    Func<Task> next
+);
